c2ec5c9563fdec61b17aae80f54c12725bb10951
Roawu <94121017+Roawu@users.noreply.github.com>
2022-12-07 12:52:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,32 +33,29 @@ def format_data(result):
     return format_result
 
 def searchby_pqe(search_pqe):
-    global initial_search_results
     for pqe in search_pqe:
         result = table_candidates.all(formula=fm.match({"PQE": pqe}))
         # print("results: "+str(len(result)))
-        initial_search_results += result
+        return result
 
 
 def searchby_job(search_jobtags):
-    global initial_search_results
     for job in search_jobtags:
         result = table_candidates.all(
             formula=fm.FIND(fm.STR_VALUE("Data Privacy & Cybersecurity"), fm.FIELD("Job Tags")))
         # print("results: "+str(len(result)))
-        initial_search_results += result
+        return result
 
 
 def searchby_location(search_location):
-    global initial_search_results
     for location in search_location:
         result = table_candidates.all(formula=fm.FIND(fm.STR_VALUE(location), fm.FIELD("Location")))
         # print("results: "+str(len(result)))
-        initial_search_results += result
+        return result
 
 
-def filter_by_pqe(result):
-    pqe = result0.get("fields").get("PQE")  # get job tag from result0
+def filter_by_pqe(result, search_pqe):
+    pqe = result.get("fields").get("PQE")  # get job tag from result0
     if (len(search_pqe) == 0):
         pqe_match = True
     elif (pqe == None):
@@ -70,8 +67,8 @@ def filter_by_pqe(result):
     return pqe_match
 
 
-def filter_by_job(result):
-    job_tags = result0.get("fields").get("Job Tags")  # get job tag from result0
+def filter_by_job(result, search_jobtags):
+    job_tags = result.get("fields").get("Job Tags")  # get job tag from result0
     if (len(search_jobtags) == 0):
         job_match = True
     elif (job_tags == None):
@@ -83,8 +80,9 @@ def filter_by_job(result):
     return job_match
 
 
-def filter_by_location(result):
-    location_tags = result0.get("fields").get("Location")  # get location tag from result0
+def filter_by_location(result, search_location):
+
+    location_tags = result.get("fields").get("Location")  # get location tag from result0
     if (len(search_location) == 0):
         match = True
     elif (location_tags == None):
@@ -100,21 +98,26 @@ def filter_by_location(result):
 def return_response():
     initial_search_results = []
     search_pqe = request.form.getlist('PQE')
+    print("pqe")
     search_jobtags = request.form.getlist('jobtags')
+    print("jobtag")
     search_location = request.form.getlist('location')
+    print("location")
+
     if (len(search_pqe) > 0):  # if search_pqe exists
-        searchby_pqe(search_pqe)
+        initial_search_results += searchby_pqe(search_pqe)
+        print("search pqe")
     elif (len(search_jobtags) > 0):
-        searchby_job(search_jobtags)
+        print("search job")
+        initial_search_results += searchby_job(search_jobtags)
     elif (len(search_location) > 0):
-        searchby_location(search_location)
+        print("search loc")
+        initial_search_results += searchby_location(search_location)
 
     # print((filter_pqe_results))
     print(len(initial_search_results))
     # print(len(filter_pqe_results))
 
-    filter_jobtags_results = []
-    filter_location_results = []
 
     correct_results = []
 
@@ -123,14 +126,14 @@ def return_response():
 
     for result0 in initial_search_results:  # for each result
         i += 1
-        pqe_match = filter_by_pqe(result0)
+        pqe_match = filter_by_pqe(result0, search_pqe)
         if (pqe_match):
 
-            job_match = filter_by_job(result0)
+            job_match = filter_by_job(result0, search_jobtags)
             if (job_match):  # if no jobtags or contains jobtag
                 # print("job match")
 
-                location_match = filter_by_location(result0)
+                location_match = filter_by_location(result0, search_location)
                 if (location_match):  # if no location tag or contains loc tag
 
                     data = format_data(result0)
